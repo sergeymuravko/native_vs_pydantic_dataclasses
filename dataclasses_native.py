19ff1c323f8e65e2364b23e84092ee0a68e7fce9
smuravko <sergeymuravko@gmail.com>
2022-03-29 21:18:56 +0300

    Add compare of converting to dict
    Compare calculation of converting to dict 1 000 000 instances of
    each type of dataclass

--- dataclasses_native.py
+++ dataclasses_native.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from utils.timeit import timeit
 
 
@@ -15,6 +15,15 @@ def creating() -> None:
         Article("New title", "Author", "python")
 
 
+@timeit
+def convert_to_dict(article: Article) -> None:
+    for i in range(1000000):
+        asdict(article)
+
+
 if __name__ == '__main__':
-    creating()
+    # creating()
     # creating 1 000 000 of instances of dataclass takes ~0.444041479990 seconds
+    first_article = Article("New title", "Author", "python")
+    convert_to_dict(first_article)
+    # Converting to dict 1 000 000 of instances of dataclass takes ~5.510993991006 seconds
--- dataclasses_pydantic.py
+++ dataclasses_pydantic.py
@@ -15,7 +15,17 @@ def creating() -> None:
         Article(**data)
 
 
+@timeit
+def convert_to_dict(article: Article) -> None:
+    for i in range(1000000):
+        article.dict()
+
+
 if __name__ == '__main__':
     data = {'title': 'New title', 'author': 'Author', 'language': 'python'}
-    creating()
+    # creating()
     # creating 1 000 000 of instances of dataclass takes ~5.404597358007 seconds
+    first_article = Article(**data)
+    convert_to_dict(first_article)
+    # Converting to dict 1 000 000 of instances of dataclass takes ~5.175302796008 seconds
+
